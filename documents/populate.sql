7cdd81e8b6915f231322e3a52f2c320d1094b3fe
johnoliveira06 <jhonoliver567@gmail.com>
2024-02-14 09:09:22 -0300

    Transaction SQL

--- documents/populate.sql
+++ documents/populate.sql
@@ -6,7 +6,6 @@ INSERT INTO Customers (name, cpf, obs) VALUES
 ('John', '12345678912', NULL),
 ('Rennan', '98765432113', NULL);
 
-
 INSERT INTO Agencies (number, description, banks_id) VALUES 
 ('1234', 'Conta corrente', 1),
 ('4321', 'Conta corrente', 2);
@@ -16,6 +15,32 @@ INSERT INTO Accounts (number, balance, agencies_id, customers_id) VALUES
 ('654321', '1000', 2, 2);
 
 INSERT INTO Transactions (date, type, amount, description, accounts_id) VALUES 
-(NOW(), 'Retirada', 50, 'Pagamento', 1),
+(NOW(), 'Saída', 50, 'Pagamento', 1),
 (NOW(), 'Entrada', 50, NULL, 2);
 
+DELIMITER //
+
+CREATE PROCEDURE transferencia(transfer_value DECIMAL(10,2))
+BEGIN
+    DECLARE original_balance DECIMAL(10,2);
+    DECLARE rollback_message VARCHAR(255) DEFAULT 'Saldo insuficiente para transferência';
+    DECLARE commit_message VARCHAR(255) DEFAULT 'Transferência realizada com sucesso!';
+    
+    START TRANSACTION;
+
+    SELECT balance INTO original_balance FROM Accounts WHERE customers_id = 1 FOR UPDATE;
+    
+    IF original_balance >= transfer_value THEN
+        UPDATE Accounts SET balance = original_balance - transfer_value WHERE customers_id = 1;
+        UPDATE Accounts SET balance = balance + transfer_value WHERE customers_id = 2;
+        INSERT INTO Transactions (date, type, amount, description, accounts_id) VALUES (NOW(), 'Saída', transfer_value, NULL, 1);
+    ELSE
+        ROLLBACK;
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = rollback_message;
+    END IF;
+
+    COMMIT;
+    SELECT commit_message as 'Resultado';
+END// 
+
+DELIMITER ;
