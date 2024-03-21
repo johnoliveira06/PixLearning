019d0be95cfb09a46023ed1326a6c822c9dbbefb
johnoliveira06 <jhonoliveira567@gmail.com>
2024-03-21 20:35:08 -0300

    style:change the home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,107 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/home.css";
-import Footer from "../components/Footer";
 import { useNavigate } from "react-router-dom";
-import ReactDOM from 'react-dom';
-
+import { getBankList } from "../services/apiServices";
 
 function Home() {
+  const [banks, setBanks] = useState([]);
   const navigate = useNavigate();
 
-  return (
-    <>
-      <div>
-        <div>Escolha seu banco</div>
-        <div>
-          <button
-            onClick={() => {
-              /* 1. Navigate to the Details route with params */
-              navigate('login', {
-                state: {
-                  bank_id: 1,
-                  name: "Banco do Brasil"
-                }
-              });
-            }}
-            >Banco do Brasil
-          </button>
-        </div>
+  async function getBanks() {
+    try {
+      const result = await getBankList();
+      setBanks(result);
+    } catch (error) {
+      console.log("Error fetching data: ", error);
+    }
+  }
 
-        <div>
-          <button
-              onClick={() => {
-                /* 1. Navigate to the Details route with params */
-                navigate('login',  {
+  useEffect(() => {
+    getBanks();
+  }, []);
+
+  const bankImages = {
+    1: "../../assets/icons/logoBB.svg",
+    2: "../../assets/icons/logoBradesco_vermelho.svg",
+  };
+
+  return (
+    <div className="container">
+      <h1>Escolha seu banco</h1>
+      <div className="content">
+        {banks.map((bank) => (
+          <div key={bank.id} className="bank-item">
+            <a
+              className="bank-link"
+              href={`/login/${bank.id}`}
+              onClick={(e) => {
+                e.preventDefault();
+                navigate("login", {
                   state: {
-                    bank_id: 2,
-                    name: "Bradesco"
-                  }
+                    bank_id: bank.id,
+                    name: bank.name,
+                  },
                 });
               }}
-            >Bradesco
-          </button>    
-        </div>
-      </div>
-      {/* <header>
-        <h1 class="logo">
-          <a href="/">SambaCrédito</a>
-        </h1>
-        <input type="checkbox" class="nav-toggle" id="nav-toggle" />
-        <nav>
-          <ul>
-            <li>
-              {" "}
-              <a href="/register">Abra sua conta</a>
-            </li>
-            <li>
-              {" "}
-              <a href="/login">Acesse sua conta</a>
-            </li>
-          </ul>
-        </nav>
-        <label for="nav-toggle" class="nav-toggle-label">
-          <span></span>
-        </label>
-      </header>
-
-      <div class="init">
-        <div class="main-container">
-          <div class="main">
-            <h1>Invista, Cresça, Conquiste.</h1>
-          </div>
-        </div>
-      </div>
-      <section class="section-one">
-        <div class="middle-container">
-          <h3>
-            Transformando o presente, moldando o futuro. Tecnologia financeira
-            que enriquece vidas.
-          </h3>
-        </div>
-        <div class="clearfix"></div>
-      </section>
-      <section class="section-two">
-        <div className="flex-group">
-          <div class="end-container">
-            <p>
-              Em 2023 o SambaCrédito registrou um resultado recorrente geral
-              impressionante de R$ 9,0 bilhões, um aumento de 3,4% em relação ao
-              mesmo período do ano anterior, demonstrando sua força e
-              estabilidade operacional mesmo em tempos de volatilidade
-              econômica.
-            </p>
-            <a
-              href="https://www.correiobraziliense.com.br/economia/2023/09/5123749-banco-do-brasil-expande-mercado-de-carbono-e-aumenta-area-protegida.html"
-              class="learn-more-btn section-four-btn"
             >
-              Saiba mais
+              <img
+                className="bank-image"
+                src={`/images/banks/${bankImages[bank.id]}`}
+                alt={bank.name}
+              />
+              {bank.name}
             </a>
-            <div class="clearfix"></div>
           </div>
-        </div>
-      </section>
-      <Footer /> */}
-    </>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,11 +4,11 @@ import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
 import "../styles/login.css";
 import { getBankList } from "../services/apiServices";
-import toast, { toastConfig } from 'react-simple-toasts';
-import 'react-simple-toasts/dist/theme/dark.css'; // choose your theme
-import 'react-simple-toasts/dist/theme/failure.css'; // choose your theme
+import toast, { toastConfig } from "react-simple-toasts";
+import "react-simple-toasts/dist/theme/dark.css"; // choose your theme
+import "react-simple-toasts/dist/theme/failure.css"; // choose your theme
 
-toastConfig({ theme: 'failure' }); // configure global toast settings, like theme
+toastConfig({ theme: "failure" }); // configure global toast settings, like theme
 
 function Login() {
   const [banks, setBanks] = useState([]);
@@ -68,7 +68,7 @@ function Login() {
       console.log(result);
       setData(result);
     } catch (error) {
-      console.log('Error fetching data: ', error)
+      console.log("Error fetching data: ", error);
     }
     console.log(data);
   }
@@ -84,83 +84,76 @@ function Login() {
   const handleNewLogin = () => {
     let url = `http://localhost:3000/accounts?filter[where][agencies_id]=${agency}&filter[where][number]=${account}&filter[where][password]=${password}`;
 
-    axios
-    .get(url)
-    .then((loginResponse) => {
+    console.log(url);
+    axios.get(url).then((loginResponse) => {
       const account = loginResponse.data;
 
       if (account.length === 1) {
-        sessionStorage.setItem('accountLogged', true);
-        sessionStorage.setItem('accountData', JSON.stringify(account[0]));
+        sessionStorage.setItem("accountLogged", true);
+        sessionStorage.setItem("accountData", JSON.stringify(account[0]));
         navigate("/account");
       } else {
         textInput.current?.focus();
-        toast('Dados incorretos! Tente novamente mais tarde.')
+        toast("Dados incorretos! Tente novamente mais tarde.");
       }
     });
-  }
+  };
 
   return (
     <>
       <Navbar />
       <div className="login">
         <h1>{bankData.name}</h1>
-          <div className="col-12">
-            <label className="form-label" htmlFor="inlineFormSelectPref">
-              Entre na sua conta digital
-            </label>
-            <input
-              type="hidden"
-              id="inlineFormSelectPref"
-              defaultValue={bankData.id}
-            />
-
-          </div>
-          <div className="col-md-6">
-            <label className="form-label">Código da Agência</label>
-            <input
-              type="text"
-              className={`form-control ${agencyError ? "is-invalid" : ""}`}
-              value={agency}
-              onChange={handleAgencyChange}
-              ref={textInput}            />
-            {agencyError && (
-              <p className="error">Agência não pode estar vazia</p>
-            )}
-          </div>
-          <div className="col-md-6">
-            <label htmlFor="inputPassword" className="form-label">
-              Número da Conta
-            </label>
-            <input
-              type="text"
-              className={`form-control ${accountError ? "is-invalid" : ""}`}
-              value={account}
-              onChange={handleAccountChange}
-            />
-            {accountError && (
-              <p className="error">Conta deve conter 6 números</p>
-            )}
-          </div>
-          <div className="col-12">
-            <label htmlFor="inputPassword" className="form-label">
-              Senha
-            </label>
-            <input
-              type="password"
-              className="form-control"
-              id="inputPassword"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-          </div>
-          <div className="col-12">
-            <button
-              onClick={handleNewLogin}
-              className="login-button"
-            >Entrar
-            </button>               
-          </div>
+        <div className="col-12">
+          <label className="form-label" htmlFor="inlineFormSelectPref">
+            Entre na sua conta digital
+          </label>
+          <input
+            type="hidden"
+            id="inlineFormSelectPref"
+            defaultValue={bankData.id}
+          />
+        </div>
+        <div className="col-md-6">
+          <label className="form-label">Código da Agência</label>
+          <input
+            type="text"
+            className={`form-control ${agencyError ? "is-invalid" : ""}`}
+            value={agency}
+            onChange={handleAgencyChange}
+            ref={textInput}
+          />
+          {agencyError && <p className="error">Agência não pode estar vazia</p>}
+        </div>
+        <div className="col-md-6">
+          <label htmlFor="inputPassword" className="form-label">
+            Número da Conta
+          </label>
+          <input
+            type="text"
+            className={`form-control ${accountError ? "is-invalid" : ""}`}
+            value={account}
+            onChange={handleAccountChange}
+          />
+          {accountError && <p className="error">Conta deve conter 6 números</p>}
+        </div>
+        <div className="col-12">
+          <label htmlFor="inputPassword" className="form-label">
+            Senha
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="inputPassword"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <div className="col-12">
+          <button onClick={handleNewLogin} className="login-button">
+            Entrar
+          </button>
+        </div>
       </div>
     </>
   );
